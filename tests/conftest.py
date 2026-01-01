a394aa4396a2bfc9adb1dab1c6c28b0c713cc95c
Tamas K Stenczel <tks32@cam.ac.uk>
2026-01-01 10:54:34 +0000

    more precision in logging disc use

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import shutil
-import pytest
 
 def pytest_runtest_logreport(report):
     if report.when == 'call':
         total, used, free = shutil.disk_usage("/")
-        print(f"\n[METRICS] {report.nodeid}: {used // (2**30)}GB used, {free // (2**30)}GB free, time: {report.duration:.2f}s")
+        print(f"\n[METRICS] {report.nodeid}: {used / (2**30):.3f}GB used, {free / (2**30):.3f}GB free, time: {report.duration:.2f}s")
